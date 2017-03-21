2e18ba9269b62921de67b3a55c8f32909109d9c4
Walsh Costigan <walshcostigan@gmail.com>
2017-03-21 16:30:37 -0400

    created db with devise, locations, and profile

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  resources :locations
+  resources :profiles
+  devise_for :users
+
+  root to: "profiles#index"
+
+  get '/admin', to: 'profiles#admin', as: '/admin'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
