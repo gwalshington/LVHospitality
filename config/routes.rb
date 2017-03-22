d5df61033076f13cc586fb195948114ce7dbe9e5
Walsh Costigan <walshcostigan@gmail.com>
2017-03-22 17:45:45 -0400

    added events table

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :events
   resources :locations
   resources :profiles
   devise_for :users
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170321202422) do
+ActiveRecord::Schema.define(version: 20170322210359) do
+
+  create_table "events", force: :cascade do |t|
+    t.datetime "date"
+    t.string   "title"
+    t.string   "subtitle"
+    t.string   "description"
+    t.integer  "user_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["user_id"], name: "index_events_on_user_id"
+  end
 
   create_table "locations", force: :cascade do |t|
     t.string   "location_name"
