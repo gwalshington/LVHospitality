b3fc98b84b7762fc6fc52621a9a05a9b8df5d47d
Walsh Costigan <walshcostigan@gmail.com>
2017-03-24 20:13:07 -0400

    trying to get weather api to work

--- app/assets/javascripts/weather.js
+++ app/assets/javascripts/weather.js
@@ -1,5 +1,5 @@
 var weatherApiKey = "1181bab21c49ceb7970b096c0cd74c56"
-var honoluluApiUrl = "http://api.openweathermap.org/data/2.5/weather?zip=96818&units=imperial&appid=1181bab21c49ceb7970b096c0cd74c56"
+var honoluluApiUrl = "https://api.openweathermap.org/data/2.5/weather?zip=96818&units=imperial&appid=1181bab21c49ceb7970b096c0cd74c56"
 var newYorkApiUrl = "http://api.openweathermap.org/data/2.5/weather?q=NewYork&units=imperial&appid=1181bab21c49ceb7970b096c0cd74c56"
 
 
@@ -9,8 +9,10 @@ $.ajax({
 	success: function(result)	{
 		var conditionImage = result.weather[0].icon
 		var temperature = Math.round(result.main.temp)
+		console.log(temperature)
 
 		$('#js_conditionImage').attr("src","/assets/weather/" + conditionImage + ".svg")
+
 		$('#js_temperature').append(temperature)
 
 		console.log(result);
