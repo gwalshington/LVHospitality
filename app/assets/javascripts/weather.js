7bfa761504855b02cccdc7a2a2aecbf323157587
Walsh Costigan <walshcostigan@gmail.com>
2017-03-31 14:59:12 -0400

    changed how weather icons are called and displayed

---
+++ app/assets/javascripts/weather.js
@@ -0,0 +1,19 @@
+
+var honoluluApiUrl = "https://api.weatherbit.io/v1.0/current?lat=21.3069&lon=-157.8583&key=873efaa027c14804ad5bd284b8cf72c0"
+
+$.ajax({
+    url: honoluluApiUrl,
+    type: 'GET',
+    success: function(result)   {
+        var conditionImage = "'#" + result.data[0].weather.icon + "'"
+        var temperature = result.data[0].rh
+
+        $('#c01d').css({"display":"inline"})
+        $('#js_temperature').append(temperature)
+
+
+    },
+    error: function(error)  {
+        console.log(error)
+    }
+})
